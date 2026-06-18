621957d838b1403f83e97646eacb29cd48745a01
Nai22112008 <naiara.aires@escola.pr.gov.br>
2026-06-18 09:32:31 -0300

    Update main.js

--- programação/main.js
+++ programação/main.js
@@ -39,4 +39,30 @@ function calculaTempo(tempoObjetivo) {
   let tempoFinal = tempoObjetivo - tempoAtual;
   let segundos = Math.floor(tempoFinal/1000);
   let minutos = Math.floor(segundos/60);
-  let horas = Math.floor(minutos
+  let horas = Math.floor(minutos/60);
+  let dias =Math.floor(horas/24);
+
+
+  segundos%=60;
+  minutos%=60;
+  horas%=24;
+  if(tempoFinal>0){
+    return[dias,horas,minutos,segundos];
+    }else{
+    return[0,0,0,0];
+  }
+}
+
+function atualizaCronometro(){
+  for(let i=0;i<contadores.lenght;i++){
+    document.getElementByld("dias"+i).textContent=calculaTempo(tempos[i])[0];
+    document.getElementByld("horas"+i).textContent=calculaTempo(tempos[i])[1];
+    document.getElementByld("min"+i).textContent=calculaTempo(tempos[i])[2];
+    document.getElementByld("seg"+i).textContent=calculaTempo(tempos[i])[3];
+  }
+}
+function comecaCronometro(){
+  atualizaCronometro();
+  setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
