9dffa5579226d2061cb043744d99a27cfa4aa422
Nai22112008 <naiara.aires@escola.pr.gov.br>
2026-06-18 09:09:27 -0300

    Update main.js

--- programação/main.js
+++ programação/main.js
@@ -8,6 +8,35 @@ botoes[j].classList.remove("ativo");
 botoes[i].classList.add("ativo");
 };
 
-
-
 }
+const botoes = document.querySelectorAll(".botao")
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i=0; i < botoes.length; i++) {
+  botoes[i].onclick = function() {
+
+for (let j = 0; j< botoes.length; j++) {
+  botoes[j].classList.remove("ativo");
+  textos[j].classList.remove("ativo");
+}
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  }
+}
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = newDate("2024-10-05T00:00:00");
+const tempoObjetivo2 = newDate("2024-12-05T00:00:00");
+const tempoObjetivo3 = newDate("2024-12-03T00:00:00");
+const tempoObjetivo4 = newDate("2025-02-01T00:00:00");
+
+const tempos= [temposObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal/1000);
+  let minutos = Math.floor(segundos/60);
+  let horas = Math.floor(minutos
